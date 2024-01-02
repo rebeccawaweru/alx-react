8f4ab5c752d8e6df7fc414e0fa75bea54e094957
rebeccawaweru <wawerur95@gmail.com>
2024-01-03 00:06:50 +0300

    function debug

--- 0x00-Webpack/README.md
+++ 0x00-Webpack/README.md
@@ -11,3 +11,7 @@ All of your code will be executed on Ubuntu 18.04 LTS using Node 12.x.x
 Allowed editors: vi, vim, emacs, Visual Studio Code
 All of your files should end with a new line
 A README.md file at the root of the folder of the project is mandatory
+
+
+In order to download image from url in ubuntu terminal:
+wget [image_url_here] -O desired_filename.extension
--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -4,10 +4,10 @@ const _ = require('lodash');
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
-$('body').append("<p id='count'></p>");
+$('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
-const updateCounter = () => {
+function updateCounter() {
     let times = $('#count').html() || 0;
     $('button').on('click', () => {
         times++;
