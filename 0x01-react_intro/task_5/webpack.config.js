75d4585fd7b5c57abf122ffe1590e1f797551fcf
rebeccawaweru <wawerur95@gmail.com>
2024-01-04 21:45:15 +0300

    webpack

---
+++ 0x01-react_intro/task_5/webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        static: './dist',
+        hot: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                   loader: 'babel-loader',
+                   options: {
+                     presents: ['@babel/present-env'],
+                   },
+                },
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader'],
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'images/',
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './dist/index.html',
+        }),
+    ],
+    devtool: 'inline-source-map',
+};
