8db13ab62feada3d22580a738f700fdc5e99602e
rebeccawaweru <wawerur95@gmail.com>
2024-01-25 13:27:43 +0300

    nested

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,5 +1,4 @@
-import { getIn, fromJS } from "./node_modules/immutable/dist/immutable";
-
-export function accessImmutableObject(object, array){
+import { getIn, fromJS } from "immutable";
+export default function accessImmutableObject(object, array){
     return getIn(fromJS(object), array);
-};
+}
