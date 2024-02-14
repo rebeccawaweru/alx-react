89f52378151b93b901572187e773d53549571654
rebeccawaweru <wawerur95@gmail.com>
2024-02-15 01:56:26 +0300

    index.js

--- 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App/App';
-import {createStore,applyMiddleware} from 'redux'
+import {createStore,applyMiddleware, compose} from 'redux'
 import {Provider} from 'react-redux'
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk'
 import rootReducer from './reducers/rootReducer';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
-  composeWithDevTools(
-    applyMiddleware(thunk)
-  )
+  composeEnhancers(applyMiddleware(thunk))
 )
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
